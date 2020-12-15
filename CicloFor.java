bab6a6fab70961fd14ea82ecbcfbde0f1dff69f6
Nicolo <nicolo@icloud.com>
2020-12-15 11:09:03 +0100

    primo test

---
+++ CicloFor.java
@@ -0,0 +1,31 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclofor;
+
+/**
+ *
+ * @author Nicolo
+ */
+public class CicloFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int contatore = 0;
+        
+        for(int i = 0; i < 10; i++){
+            
+            contatore++;
+            
+        } 
+        System.out.println(contatore);
+    }
+    
+}
